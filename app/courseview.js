25b4e52362483ae89785097741130154d1330c92
Jordan Hart <jordan@localhost.localdomain>
2016-08-12 00:18:50 -0700

    started listview

--- app/courseview.js
+++ app/courseview.js
@@ -2,52 +2,36 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
   TouchableOpacity,
 } from 'react-native';
+import { Header, Title, Container, Content, List, ListItem, Text } from 'native-base';
+
 
 export default class MentorTutoring extends Component {
   constructor(props) {
     super(props);
+    //listview getInitialState
 
-    this.state = {};
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    alignSelf: 'stretch',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+          var items = ['Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can'];
+          return (
+              <Container>
+              <Header>
+                    <Title>Tutoring List</Title>
+                </Header>
+                  <Content>
+                      <List dataArray={items}
+                          renderRow={(item) =>
+                              <ListItem>
+                                  <Text>{item}</Text>
+                              </ListItem>
+                          }>
+                      </List>
+                  </Content>
+              </Container>
+          );
+      }
+    }
