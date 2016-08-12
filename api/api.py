eec9745b3947c1efd61e5f6b98a2454b5b493b5d
Cameron <cameron.farzaneh@gmail.com>
2016-08-11 23:28:58 -0700

    Add flask api backend

---
+++ api/api.py
@@ -0,0 +1,21 @@
+from flask import Flask,jsonify
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'mentortutoringdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mentortutoringdb'
+
+mongo = PyMongo(app)
+
+@app.route('/newuser/<a>/<b>',methods= ['GET'])
+
+def newacc(a,b):
+	#result = str(a) + str(b) + str(c)
+	#return jsonify({'name':str(a), 'email':str(b), 'googleauth':str(c)})
+
+	user = mongo.db.mentortutoringdb
+	user.insert({'name' : str(a), 'email' : str(b)})
+	return 'Added user'
+
+app.run(debug=True)
